b37a61021d07346cffe0227e64fb8ad9cf3fedd9
Sasha <55252065+Scall01@users.noreply.github.com>
2020-11-04 22:04:08 -0800

    Initial test for front-end / back-end cohesion

--- web/script.js
+++ web/script.js
@@ -1,3 +1,16 @@
+let terms;  // dictionary mapping GenZ word to standard english translation
+main()
+
+
+function main() {
+  /**
+   * Retrieves the terms from the back-end
+   */
+  let response = fetch("../api/GetTranslations")
+  let data = response.json()
+  console.log(data)
+}
+
 function convert() {
   /**
    * Converts the current input and sets the text of the output-box
